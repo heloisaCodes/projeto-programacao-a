f4c217eff2ffb868bc428d0013e6b572d208ecbe
Camilla <camillasouto18@gmail.com>
2026-07-11 04:17:23 -0300

    como vai ficar a classse selecao
    ainda nao ta pronto

---
+++ src/dollpaint/controlador/caixa_de_ferramentas/selecao.py
@@ -0,0 +1,27 @@
+from caixa_de_ferramentas.ferramentas import ferramenta
+
+
+class selecao(ferramenta):
+
+    # dentro de cada figura vai ter um contem
+    def ao_clicar(self, event, controladordesenho):
+        self.pontos=[event.x,event.y]
+        for f in controladordesenho.figuras:
+          if f.conten(event.x,event.y):
+            controladordesenho.figura_selecionada=f
+            break
+          
+    def ao_mover(self, event, controladordesenho):
+       if controladordesenho.figura_selecionada:
+          # so calculo basico ate agora
+          dx= event.x - event[0]
+          dy=event.y - event[1]
+          controladordesenho.figura_selecionada.mover(dx,dy)
+
+    def ao_soltar(self, event, controladordesenho):
+       controladordesenho.figura_selecionada=None
+          
+             
+        
+
+
